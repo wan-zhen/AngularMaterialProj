6c6b9c2cb2524a02c0b6d968bcb1b40e48273511
wan-zhen <kelly811124811124@hotmail.com>
2018-02-08 17:16:16 +0800

    更新angular CLI版本，引入material Button 模組拆至共用module，play button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatButtonModule} from '@angular/material';
+import { SharedComponent } from './shared/shared.component';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  //  MatButtonModule,
+    SharedModule // 抽共用 Material 模組用
   ],
   providers: [],
   bootstrap: [AppComponent]
