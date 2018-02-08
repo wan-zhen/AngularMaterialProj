38dfcd7a3c296a309a032ff377790b6015adbd42
wan-zhen <kelly811124811124@hotmail.com>
2018-02-08 17:32:33 +0800

    play mat-icon (Day3)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
+import {MatIconModule} from '@angular/material';
 // 共用模組
 @NgModule({
-  imports: [MatButtonModule], // 先import
-  exports: [MatButtonModule] // 在export
+  // 先import
+  imports: [MatButtonModule, 
+  MatIconModule],
+  // 再export
+  exports: [MatButtonModule, 
+  MatIconModule]
 })
 export class SharedModule {}
