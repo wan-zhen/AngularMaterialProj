01bc75551730720cd97bf06237eded93c311a4dc
wan-zhen <kelly811124811124@hotmail.com>
2018-02-09 17:32:27 +0800

    嘗試拆路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-//import {MatButtonModule} from '@angular/material';
+
 import { SharedComponent } from './shared/shared.component';
 import { SharedModule } from './shared/shared.module';
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent
+    SharedComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-  //  MatButtonModule,
-    SharedModule // 抽共用 Material 模組用
+    SharedModule, // 抽共用 Material 模組用
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
