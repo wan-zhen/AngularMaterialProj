21f6a173e1b9588127022cd28a2c249b2a9e34fe
wan-zhen <kelly811124811124@hotmail.com>
2018-02-09 16:36:28 +0800

    play button (day4)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
 import {MatIconModule} from '@angular/material';
+import {MatButtonToggleModule} from '@angular/material';
 // 共用模組
 @NgModule({
   // 先import
-  imports: [MatButtonModule, 
-  MatIconModule],
+  imports: [
+    MatButtonModule, 
+    MatIconModule,
+    MatButtonToggleModule],
   // 再export
-  exports: [MatButtonModule, 
-  MatIconModule]
+  exports: [
+    MatButtonModule, 
+    MatIconModule,
+    MatButtonToggleModule]
 })
 export class SharedModule {}
