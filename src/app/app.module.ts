33a001e7b81c38ebb80a7d879c6e03252ead2fe7
wan-zhen <kelly811124811124@hotmail.com>
2018-02-21 17:04:07 +0800

    終於把ui router掛進來拉

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // 路由
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -15,6 +15,15 @@ import { SharedComponent } from './shared/shared.component';
 import { SharedModule } from './shared/shared.module';
 import { FormComponent } from './form/form.component';
 
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { UIRouterModule } from '@uirouter/angular';
+
+const dashboard = { name: 'dashboard', url: '/dashboard', component: DashboardComponent };
+const button = { name: 'button', url: '/button', component: ButtonComponent };
+const selectionList = { name: 'selectionList', url: '/selectionList', component: SelectionListComponent };
+const stepper = { name: 'stepper', url: '/stepper', component: StepperComponent };
+const form = { name: 'form', url: '/form', component: FormComponent };
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,11 +36,12 @@ import { FormComponent } from './form/form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule, 原生路由
     BrowserAnimationsModule,
-    SharedModule // 抽共用 Material 模組用
+    SharedModule, // 抽共用 Material 模組用
+    UIRouterModule.forRoot({ states: [dashboard, button, selectionList, stepper, form], useHash: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
