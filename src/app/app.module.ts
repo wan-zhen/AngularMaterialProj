ae05b07dc94a9dbda6d69721aa771c91e95196b2
wan-zhen <kelly811124811124@hotmail.com>
2018-02-22 17:03:26 +0800

    長狀態時間條拉~

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector, Injectable } from '@angular/core';
+import { HttpModule } from '@angular/http';
 // 路由
 // import { AppRoutingModule } from './app-routing.module';
 
@@ -8,6 +9,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ButtonComponent } from './button/button.component';
 import { SelectionListComponent } from './selection-list/selection-list.component';
 import { StepperComponent } from './stepper/stepper.component';
+import { PeopleComponent } from './people/people.component';
+import { PersonComponent } from './person/person.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,54 +19,82 @@ import { SharedModule } from './shared/shared.module';
 import { FormComponent } from './form/form.component';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { UIRouterModule, UIRouter } from '@uirouter/angular';
-import { StateTree } from '@uirouter/visualizer';
+import { UIRouterModule, UIRouter, Transition } from '@uirouter/angular';
+import { StateTree, visualizer } from '@uirouter/visualizer';
 import { PeopleService } from '../shared/people.service';
 
-const dashboard = { name: 'dashboard', url: '/dashboard', component: DashboardComponent };
+
+const INITIAL_COMPONENTS = [AppComponent, SharedComponent, DashboardComponent, ButtonComponent,
+  SelectionListComponent, StepperComponent, FormComponent, PeopleComponent, PersonComponent];
+
+const dashboard = {
+  name: 'dashboard', url: '/dashboard', component: DashboardComponent,
+  resolve: [
+    {
+      token: 'people',
+      deps: [PeopleService],
+      resolveFn: (peopleSvc) => peopleSvc.getAllPeople()
+    }
+  ]
+};
 const button = { name: 'button', url: '/button', component: ButtonComponent };
 const selectionList = { name: 'selectionList', url: '/selectionList', component: SelectionListComponent };
 const stepper = { name: 'stepper', url: '/stepper', component: StepperComponent };
 const form = { name: 'form', url: '/form', component: FormComponent };
-
+const people = {
+  name: 'people', url: '/people', component: PeopleComponent,
+  resolve: [
+    {
+      token: 'people',
+      deps: [PeopleService],
+      resolveFn: (peopleSvc) => peopleSvc.getAllPeople()
+    }
+  ]
+};
+const person = {
+  name: 'person', url: '/people/:personId', component: PeopleComponent,
+  resolve: [
+    {
+      token: 'person',
+      deps: [Transition, PeopleService],
+      resolveFn: (trans, peopleSvc) => peopleSvc.getPerson(trans.params().personId)
+    }
+  ]
+};
 @NgModule({
-  declarations: [
-    AppComponent,
-    SharedComponent,
-    DashboardComponent,
-    ButtonComponent,
-    SelectionListComponent,
-    StepperComponent,
-    FormComponent
-  ],
+  declarations: INITIAL_COMPONENTS,
   imports: [
     BrowserModule,
     // AppRoutingModule, 原生路由
+    HttpModule,
     BrowserAnimationsModule,
     SharedModule, // 抽共用 Material 模組用
     UIRouterModule.forRoot(
       {
-        states: [dashboard, button, selectionList, stepper, form],
-        useHash: false,
+        states: [dashboard, button, selectionList, stepper, form, people, person],
+        useHash: true,
         config: uiRouterConfigFn
       }
     )
   ],
-  providers: [],
+  providers: [
+    { provide: PeopleService, useClass: PeopleService }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
 /** UIRouter Config */
 export function uiRouterConfigFn(router: UIRouter, injector: Injector) {
-  // const peopleService = injector.get(PeopleService);
-
-  // peopleService.getAllPeople();
-
+  const peopleService = injector.get(PeopleService);
+  peopleService.getAllPeople();
+  console.log(peopleService.getAllPeople());
   // If no URL matches, go to the `dashboard` state by default
   router.urlService.rules.otherwise({ state: 'dashboard' });
 
-  console.log(document.getElementById('statetree'));
+  // 長時間條的
+  visualizer(router);
   // Use ui-router-visualizer to show the states as a tree
+  // 長樹的
   StateTree.create(router, document.getElementById('statetree'), { width: 400, height: 300 });
 }
--- src/shared/people.service.ts
+++ src/shared/people.service.ts
@@ -1,16 +1,21 @@
 import { Injectable, Inject } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
 import { Http } from '@angular/http';
 
 @Injectable()
 export class PeopleService {
+  // Plunker embeds time out. So fetch and cache the data when the app loads
+  // Continue to return observable.
   cache = null;
+
   constructor(@Inject(Http) public http: Http) {
-    console.log('People Service constructor');
+    console.log('PeopleService constructor');
   }
 
   getAllPeople() {
     return this.cache = this.cache || this.http.get('./assets/peoples.json')
-      // .map<any>(resp => resp.json())
+      // .map(resp => resp.json())
       .toPromise();
   }
 
